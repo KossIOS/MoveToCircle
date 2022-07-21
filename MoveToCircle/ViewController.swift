640a15639569782a7eb8ad34b8add13b0b087446
Koss <102085029+KossIOS@users.noreply.github.com>
2022-07-22 00:44:33 +0200

    test commit

--- MoveToCircle/ViewController.swift
+++ MoveToCircle/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var circleView = makeCircle()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture))
+        view.addGestureRecognizer(panGesture)
+        view.addSubview(circleView)
+        circleView.isHidden = true
+        view.backgroundColor = .black
+        
     }
-
-
+    
+    private func makeCircle() -> UIView {
+        let circleView = UIView()
+        circleView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        circleView.layer.cornerRadius = circleView.frame.width / 2
+        circleView.backgroundColor = .orange
+        return circleView
+        
+    }
+    
+    @objc
+    private func handlePanGesture(_ sender: UIPanGestureRecognizer) {
+        circleView.center = sender.location(in: sender.view)
+        circleView.isHidden = false
+        
+    }
 }
+
 
